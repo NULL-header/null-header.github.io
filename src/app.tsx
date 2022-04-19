c2dd83f288071dadfd60e8046d94e6170d71f770
NULL-header <poaoq.poaoq@gmail.com>
2022-04-19 13:13:23 +0900

    style: remove margin and padding #5

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,22 @@
 import React from "react";
+import { extendTheme, Box, ChakraProvider } from "@chakra-ui/react";
 import { HelloWorld } from "./components/HelloWorld";
 
-export const App = () => <HelloWorld />;
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        margin: 0,
+        padding: 0,
+      },
+    },
+  },
+});
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box height="100vh" width="100vw">
+      <HelloWorld />
+    </Box>
+  </ChakraProvider>
+);
