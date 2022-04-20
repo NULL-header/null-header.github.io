ece7ee54cf84552ccf388667bb5d901f3b8963f9
NULL-header <poaoq.poaoq@gmail.com>
2022-04-20 21:41:41 +0900

    feat: nested animation #6

--- src/components/content-tree/tree.tsx
+++ src/components/content-tree/tree.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { keyframes, Box } from "@chakra-ui/react";
+import { motion, AnimatePresence } from "framer-motion";
 import { ItemProps, Item } from "./item";
 import { Menu } from "./menu";
 
@@ -12,28 +14,51 @@ type Contents = {
 interface TreeProps {
   contents: Contents;
 }
-
 export const Tree = ({ contents }: TreeProps) => {
   const [levels, setLevels] = useState([contents]);
-  const level = levels[levels.length - 1];
-  const record = Object.entries(level).reduce((a, [key, val]) => {
-    if (val.isContent) {
-      const content = (props: ItemProps) => <Item {...props} />;
-      // eslint-disable-next-line no-param-reassign
-      a[key] = content;
-    } else {
-      const folder = (props: ItemProps) => (
-        <Item
-          {...props}
-          onClick={() => {
-            setLevels((nextLevels) => [...nextLevels, val.under]);
-          }}
-        />
-      );
-      // eslint-disable-next-line no-param-reassign
-      a[key] = folder;
-    }
-    return a;
-  }, {} as Record<string, React.FC<ItemProps>>);
-  return <Menu itemRecord={record} />;
+  const makeRecord = useCallback(
+    (level: Contents) =>
+      Object.entries(level).reduce((a, [key, val]) => {
+        if (val.isContent) {
+          const content = (props: ItemProps) => <Item {...props} />;
+          // eslint-disable-next-line no-param-reassign
+          a[key] = content;
+        } else {
+          const folder = (props: ItemProps) => (
+            <Item
+              {...props}
+              onClick={() => {
+                setLevels((lastLevels) => [...lastLevels, val.under]);
+              }}
+            />
+          );
+          // eslint-disable-next-line no-param-reassign
+          a[key] = folder;
+        }
+        return a;
+      }, {} as Record<string, React.FC<ItemProps>>),
+    [],
+  );
+
+  return (
+    <Box
+      outline="solid"
+      width="80%"
+      height="80%"
+      overflow="hidden"
+      display="grid"
+      gridTemplateColumns="1fr"
+      sx={{
+        "*": {
+          gridRowStart: 1,
+          gridColumnStart: 1,
+        },
+      }}
+    >
+      <Menu
+        itemRecord={makeRecord(levels[levels.length - 1])}
+        level={levels.length}
+      />
+    </Box>
+  );
 };
