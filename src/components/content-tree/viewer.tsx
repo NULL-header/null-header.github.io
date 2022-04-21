ddf18c04a1f8d2805cdf50e0a8bb3ed8c6c197a8
NULL-header <poaoq.poaoq@gmail.com>
2022-04-21 14:01:35 +0900

    build: page #17

--- src/components/content-tree/viewer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-import { AnimatePresence, motion } from "framer-motion";
-
-export const Viewer = () => (
-  <Box bg="tomato" height="80%" width="80%">
-    View
-  </Box>
-);
