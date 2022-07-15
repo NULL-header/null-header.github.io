2ad2638ffdc901d655cb3ee00873b5840c32e2f5
NULL-header <poaoq.poaoq@gmail.com>
2022-07-16 04:57:20 +0900

    fix: putting icons position #45

--- components/content-list/links/base/external-link.tsx
+++ components/content-list/links/base/external-link.tsx
@@ -6,8 +6,16 @@ import { LinkProps } from "./type";
 
 export const ExternalLink = React.memo(({ title, link, icon }: LinkProps) => (
   <BaseInterface icon={icon}>
-    <Link flexGrow={1} _focus={{ outline: "none" }} href={link} isExternal>
-      <Text>{title}</Text>
+    <Link
+      flexGrow={1}
+      _focus={{ outline: "none" }}
+      href={link}
+      isExternal
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+    >
+      <Text flexGrow={1}>{title}</Text>
       <Icon as={FaExternalLinkAlt} />
     </Link>
   </BaseInterface>
