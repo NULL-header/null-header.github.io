6d00e4ee97f2413372f90e9d3f408ae405d93034
NULL-header <poaoq.poaoq@gmail.com>
2022-07-16 05:19:41 +0900

    fix: stying #45

--- components/content-list/container/container.tsx
+++ components/content-list/container/container.tsx
@@ -6,6 +6,9 @@ import {
   AccordionButton,
   AccordionIcon,
   Text,
+  Box,
+  Divider,
+  Icon,
 } from "@chakra-ui/react";
 import { BaseInterface, BaseInterfaceProps } from "../interface";
 
@@ -13,17 +16,48 @@ interface ContainerProps extends BaseInterfaceProps {
   title: string;
   children: JSX.Element | JSX.Element[];
 }
+// copy & paste occurred
+// for css styling
 export const Container = React.memo(
   ({ title, children, icon }: ContainerProps) => (
     <Accordion allowToggle>
       <AccordionItem borderStyle="unset">
-        <AccordionButton width="100%">
-          <BaseInterface icon={icon}>
-            <Text textAlign="left" flexGrow={1}>
-              {title}
-            </Text>
-            <AccordionIcon />
-          </BaseInterface>
+        <AccordionButton
+          height="7vh"
+          width="100%"
+          display="flex"
+          flexDirection="row"
+          borderColor="white"
+          borderStyle="solid"
+          borderWidth="medium"
+          borderRadius="full"
+          textColor="white"
+          color="white"
+          alignItems="center"
+          padding="2"
+          paddingX="4"
+          _hover={{
+            backgroundColor: "white",
+            textColor: "black",
+            color: "black",
+          }}
+          _focus={{
+            backgroundColor: "white",
+            textColor: "black",
+            color: "black",
+          }}
+        >
+          <Icon as={icon} />
+          <Divider
+            orientation="vertical"
+            margin="2%"
+            height="100%"
+            borderLeftWidth="2px"
+          />
+          <Text textAlign="left" flexGrow={1}>
+            {title}
+          </Text>
+          <AccordionIcon />
         </AccordionButton>
         <AccordionPanel>{children}</AccordionPanel>
       </AccordionItem>
