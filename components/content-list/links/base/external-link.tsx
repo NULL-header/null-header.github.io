995214f3d3f26d00d92d8a8e05ff3e09f808b2f8
NULL-header <poaoq.poaoq@gmail.com>
2022-07-16 02:13:35 +0900

    feat: add icon #45

--- components/content-list/links/base/external-link.tsx
+++ components/content-list/links/base/external-link.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, Text } from "@chakra-ui/react";
+import { Link, Text, Icon } from "@chakra-ui/react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import { BaseInterface } from "../../interface";
 import { LinkProps } from "./type";
 
@@ -7,6 +8,7 @@ export const ExternalLink = React.memo(({ title, link, icon }: LinkProps) => (
   <BaseInterface icon={icon}>
     <Link flexGrow={1} _focus={{ outline: "none" }} href={link} isExternal>
       <Text>{title}</Text>
+      <Icon as={FaExternalLinkAlt} />
     </Link>
   </BaseInterface>
 ));
